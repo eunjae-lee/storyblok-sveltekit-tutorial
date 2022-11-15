fac280b2294a195221da02166b11a97f539a68a7
Eunjae Lee <hey@eunjae.dev>
2022-11-15 11:37:36 +0100

    chore: initial commit

---
+++ src/components/Page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { storyblokEditable } from '@storyblok/svelte';
+
+	export let blok;
+</script>
+
+<div use:storyblokEditable={blok} class="py-8 mb-6 text-5xl font-bold text-center">page</div>
